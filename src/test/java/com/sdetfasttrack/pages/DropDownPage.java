b057ed40bea6fe1f0f4c44b85cbc6e8cea7da3e8
ugurargun <sarpercaglayan16@gmail.com>
2021-10-10 04:47:33 +0300

    DropdownPage has added

---
+++ src/test/java/com/sdetfasttrack/pages/DropDownPage.java
@@ -0,0 +1,41 @@
+package com.sdetfasttrack.pages;
+import com.sdetfasttrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class DropDownPage {
+    public DropDownPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+    @FindBy(id = "state")
+    public WebElement stateSelector;
+
+    @FindBy(xpath = "//select[@id='day']")
+    public WebElement daySelector;
+
+    @FindBy(xpath = "//select[@id='month']")
+    public WebElement monthSelector;
+
+    @FindBy(xpath = "//select[@id='year']")
+    public WebElement yearSelector;
+
+}
+
+
+    //TC#2
+    //Select “December 1st, 1921” and verify it is selected.
+    //1. Go to http://practice.cybertekschool.com/dropdown
+    //2. Select Year --> by visible text
+    //3. Select Month -->  by value
+    //4. Select Day -->  by index
+    //5. Verify final selected option is California.
+
+    //TC#3
+    //1.Go to http://practice.cybertekschool.com/dropdown
+    //2. Select all the options from multiple select dropdown.
+    //3. Print out all selected values and verify each is selected.
+    //4.Deselect all values and verify each is deselected.
+
+
+
