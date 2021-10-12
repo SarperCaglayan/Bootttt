ec8eb654c674fd5e588b0c6b4e3a5a1fa3f08297
ugurargun <sarpercaglayan16@gmail.com>
2021-10-13 00:56:01 +0300

    iframe tests have updated

--- src/test/java/com/sdetfasttrack/pages/AlertClass.java
+++ src/test/java/com/sdetfasttrack/pages/AlertClass.java
@@ -1,13 +1,23 @@
 package com.sdetfasttrack.pages;
 
 import com.sdetfasttrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AlertClass {
-    public AlertClass(){
+    public AlertClass() {
         PageFactory.initElements(Driver.getDriver(), this);
-
-
     }
+    @FindBy(xpath = "//button[.='Click for JS Alert']")
+    public WebElement clickJSAlert;
 
+    @FindBy(xpath = "//button[.='Click for JS Confirm']")
+    public WebElement clickJSConfirm;
+
+    @FindBy(xpath = "//button[.='Click for JS Prompt']")
+    public WebElement clickJSPrompt;
+
+    @FindBy(id = "result")
+    public WebElement textResult;
 }
--- src/test/java/com/sdetfasttrack/tests/AlertClassTests.java
+++ src/test/java/com/sdetfasttrack/tests/AlertClassTests.java
@@ -1,15 +1,49 @@
 package com.sdetfasttrack.tests;
 
+import com.sdetfasttrack.pages.AlertClass;
 import com.sdetfasttrack.utilities.Driver;
+import org.openqa.selenium.Alert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class AlertClassTests {
 
+    AlertClass alertClass = new AlertClass();
+
     //1. Go to website: http://practice.cybertekschool.com/javascript_alerts
-    //2.Clicking to the button for alert to be displayed
-    //3. Click to OK button from the alert
-    //4. Verify "You successfully clicked an alert" text is displayed.
+    @Test
+    public void JSAlertTest1() {
 
+        Driver.getDriver().get("http://practice.cybertekschool.com/javascript_alerts");
+        //2.Clicking to the button for alert to be displayed
+        alertClass.clickJSAlert.click();
 
+        Alert alert1 = Driver.getDriver().switchTo().alert();
+        alert1.accept();
+        //4. Verify "You successfully clicked an alert" text is displayed.
+        String expected = "You successfuly clicked an alert";
+        String actual = alertClass.textResult.getText();
+
+        assertEquals(expected, actual);
+
+        //3. Click to OK button from the alert
+        alertClass.clickJSConfirm.click();
+        Alert alert2 = Driver.getDriver().switchTo().alert();
+        alert2.accept();
+        expected = "You clicked: Ok";
+        actual = alertClass.textResult.getText();
+
+        assertEquals(expected, actual);
+
+        alertClass.clickJSPrompt.click();
+        Alert alert3 = Driver.getDriver().switchTo().alert();
+        alert3.sendKeys("hello");
+        alert3.accept();
+        expected = "You entered: hello";
+        actual = alertClass.textResult.getText();
+
+        assertEquals(expected, actual);
     }
+}
 
