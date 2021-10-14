3f5fcb374224ac70e928af0588576e36cfd26bfb
ugurargun <sarpercaglayan16@gmail.com>
2021-10-15 01:27:05 +0300

    JavaFaker tests have executed

--- src/test/java/com/sdetfasttrack/tests/JavaFakerTests.java
+++ src/test/java/com/sdetfasttrack/tests/JavaFakerTests.java
@@ -1,4 +1,29 @@
 package com.sdetfasttrack.tests;
 
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
 public class JavaFakerTests {
+    @Test
+    public void testJavaFaker(){
+
+        Faker faker= new Faker();
+        String firstName= faker.name().firstName();
+        System.out.println("firstName = " + firstName);
+        String lastName= faker.name().lastName();
+        System.out.println("lastName = " + lastName);
+        String fullName= faker.name().fullName();
+        System.out.println("fullName = " + fullName);
+        String address= faker.address().fullAddress();
+
+        System.out.println("address = " + address);
+        String phoneNumber= faker.phoneNumber().phoneNumber();
+        System.out.println("phoneNumber = " + phoneNumber);
+        String creditCard= faker.business().creditCardNumber();
+        System.out.println("creditCard = " + creditCard);
+        String emailAddress= faker.internet().emailAddress();
+
+
+
+    }
 }
